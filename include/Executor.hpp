f872a11e6c8c48d0e3a88feba1233995b4d6458b
WLC <2958251898@qq.com>
2025-10-23 22:05:51 +0800

    nothing

--- include/Executor.hpp
+++ include/Executor.hpp
@@ -15,10 +15,16 @@ namespace adas
     {
     public:
         static Executor *NewExecutor(const Pose &pose = {0, 0, 'N'}) noexcept;
+
+    public:
         Executor() = default;                           // 默认构造函数
         virtual ~Executor() = default;                  // 默认虚析构函数
         Executor(const Executor &) = delete;            // 不允许拷贝
         Executor &operator=(const Executor &) = delete; // 不允许赋值
-        virtual Pose Query(void) const noexcept = 0;    // 查询当前汽车状态，纯虚函数
+
+    public:
+        // 查询当前汽车状态，纯虚函数
+        virtual Pose Query(void) const noexcept = 0;
+        // 执行一个用字符串表示的指令序列，如MLMRM，纯虚函数
     };
 }
